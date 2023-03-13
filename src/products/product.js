aa871e3e24f26f2247310de8e37621959c3129d9
Nathanael-R <nathanielwealth20@gmail.com>
2023-03-13 16:14:50 +0000

    Cleaned up recurring code and mapped over them instead

---
+++ src/products/product.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        id: 1,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 2,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 3,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 4,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 5,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 6,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 7,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    },
+    {
+        id: 8,
+        image: "https://i.ibb.co/2Ns9yqc/redShoe2.png",
+        title: 'AIR ZOOM',
+        desc: 'MENS SHOES',
+        price: 100,
+    }
+]
